3bd2bb3fcf0d739454a880ad0d33183a49e8d4dc
kadil <kadi.liimand@icefire.ee>
2021-04-20 18:41:23 +0300

    Initial commit

---
+++ src/main/java/com/kadi/WeatherStatistic/WeatherStatisticApplication.java
@@ -0,0 +1,13 @@
+package com.kadi.WeatherStatistic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherStatisticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherStatisticApplication.class, args);
+	}
+
+}
